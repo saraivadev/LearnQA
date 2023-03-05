34a7923fd78f4ab447aa36175defbe748aead057
saraivadev <wsaraiva@outlook.com>
2023-03-05 18:50:17 -0300

    Desafio QA

---
+++ primeiro.projeto/src/main/java/curso/Crud.java
@@ -0,0 +1,33 @@
+package curso;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Crud {
+
+	
+	static String disciplina;
+	
+	Scanner scaner = new Scanner(System.in);
+
+	
+	public static void	Listar(ArrayList<String> cursos){
+		System.out.println("Cursos cadastrados: \n" + cursos);
+
+		
+	}
+	
+	public static void	Excluir(){
+		
+		
+		System.out.println("Cursos excluído com sucesso!: \n");
+
+
+		
+	}
+	
+	public static void Cadastrar(){
+		System.out.println("Cursos excluído com sucesso!: \n");
+
+		
+	}}
